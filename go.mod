339b7bdcbb6f2622452427b4cb844c01b7308e15
daida <daida459031925@163.com>
2022-03-21 21:47:34 +0800

    common 雪花id 提交

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/daida459031925/common
 
 go 1.18
+
+require github.com/yitter/idgenerator-go v1.2.1
--- time/main.go
+++ time/main.go
@@ -3,8 +3,75 @@ package time
 import "time"
 
 /**
+时间结构体
+*/
+type Date struct {
+	t time.Time
+}
+
+func (date *Date) Init() (Date, error) {
+	d := Date{
+		t: time.Now(),
+	}
+	return d, nil
+}
+
+/**
 获取日期
 */
-func GetDay() {
-	time.Now().Day()
+func (date *Date) getDay() int {
+	return date.t.Day()
+}
+
+/**
+获取分钟
+*/
+func (date *Date) getMinute() int {
+	return time.Now().Minute()
+}
+
+/**
+获取月份
+*/
+func (date *Date) getMonth() time.Month {
+	return time.Now().Month()
+}
+
+func monthToInt(month string) {
+	switch month {
+	case "January":
+		break
+	case "February":
+		break
+	case "March":
+		break
+	case "April":
+		break
+	case "May":
+		break
+	case "June":
+		break
+	case "July":
+		break
+	case "August":
+		break
+	case "September":
+		break
+	case "October":
+		break
+	case "November":
+		break
+	case "December":
+		break
+	default:
+		break
+	}
+
+}
+
+/**
+获取年分
+*/
+func GetYear() {
+	time.Now().Year()
 }
