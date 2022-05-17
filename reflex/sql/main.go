0128aaf988c8767a08ce96ce024dd88efb6098ea
daida <daida459031925@163.com>
2022-05-17 08:55:54 +0800

    提交反射内容测试完毕可正常达到 获取key value 字符串数组  部分方法内部使用

--- reflex/sql/main.go
+++ reflex/sql/main.go
@@ -25,7 +25,7 @@ func RawField(in any, index int) ([]string, error) {
 	//如果时结构体
 	if v.Kind() == reflect.Struct {
 		//panic(fmt.Errorf("ToMap only accepts structs; got %T", v))
-		return GetNumField(v, index), nil
+		return getNumField(v, index), nil
 	}
 
 	//fmt.Sprintf("field %d, type %s, key %s ,value %v",
@@ -38,13 +38,13 @@ func GetField(in reflect.Value, index int) []string {
 	out := make([]string, 0)
 	for i := 0; i < in.Len(); i++ {
 		value := in.Index(i)
-		out = append(GetNumField(value, index))
+		out = append(getNumField(value, index))
 	}
 	return out
 }
 
 //返还struce 类型所有0:key 1:value
-func GetNumField(value reflect.Value, index int) []string {
+func getNumField(value reflect.Value, index int) []string {
 	out := make([]string, 0)
 	//找到这个对象的类型
 	typ := value.Type()
@@ -65,27 +65,27 @@ func GetNumField(value reflect.Value, index int) []string {
 			// `db:",type=char,length=16"`
 			switch index {
 			case 0:
-				out = GetStructKey(value, fi.Name, tagv, out)
+				out = getStructKey(value, fi.Name, tagv, out)
 			case 1:
-				out = GetStructValue(value, fi.Name, out)
+				out = getStructValue(value, fi.Name, out)
 			}
 		}
 	}
 	return out
 }
 
-func GetStructKey(value reflect.Value, fiName string, tagv string, out []string) []string {
+func getStructKey(value reflect.Value, fiName string, tagv string, out []string) []string {
 	//如果里面返还的是结构体，那么执行里面内容
-	a, b, _ := IsStructValue(value, fiName)
+	a, b, _ := isStructValue(value, fiName)
 	if a || b {
 		out = append(out, tagv)
 	}
 	return out
 }
 
-func GetStructValue(value reflect.Value, fiName string, out []string) []string {
+func getStructValue(value reflect.Value, fiName string, out []string) []string {
 	//这里面就有三种状态了 1.struct对象返还内容 2.基本类型返还值  3.struct对象内容返还
-	tag, b, val := IsStructValue(value, fiName)
+	tag, b, val := isStructValue(value, fiName)
 	if tag {
 		out = append(out, fmt.Sprintf("%v", value.FieldByName(fiName)))
 	}
@@ -99,7 +99,7 @@ func GetStructValue(value reflect.Value, fiName string, out []string) []string {
 
 //针对db内容
 //第一个返回值当前这个对象是基本类型，第二个返回值 这个对象参数是否可用， 第三个参数对象返还值内容
-func IsStructValue(value reflect.Value, fiName string) (bool, bool, string) {
+func isStructValue(value reflect.Value, fiName string) (bool, bool, string) {
 	if value.FieldByName(fiName).Kind() == reflect.Struct {
 		//如果里面返还的是结构体，那么执行里面内容
 		refStruct := value.FieldByName(fiName)
