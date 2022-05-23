7c14fe29c8ff18ce6189ece91279f7b9fb2cf74e
daida <daida459031925@163.com>
2022-05-23 21:28:59 +0800

    获取运行时的状态

---
+++ runtimeStatus/main.go
@@ -0,0 +1,28 @@
+package runtimeStatus
+
+import (
+	"fmt"
+	"runtime"
+)
+
+//获取运行时的状态
+
+func GetCurrentStatus() string {
+	return GetStatus(0)
+}
+
+func GetStatus(status int) string {
+	start := ""
+	if pc, file, line, ok := runtime.Caller(status); ok {
+		f := runtime.FuncForPC(pc)
+		//这里是打印错误，还可以进行报警处理，例如微信，邮箱通知
+		//panic(err)//退出程序
+		start = fmt.Sprintf("异常信息: ？？:%s、文件位置:%s、行数：%d、方法名称：%s、errpr信息：%s", pc, file, line, f.Name())
+	}
+
+	return start
+}
+
+func GetErrorStatus(status int, err error) string {
+	return fmt.Sprintf("%s、errpr信息：%s", GetStatus(status), err)
+}
