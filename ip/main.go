0fb6a789a2a889181e7cc4059d0cf80f295f8068
daida <daida459031925@163.com>
2022-09-18 08:46:46 +0800

    CreateIp => NewIp

--- ip/main.go
+++ ip/main.go
@@ -5,7 +5,7 @@ import (
 )
 
 // 创建IP地址
-func CreateIp(a, b, c, d byte, port int) net.TCPAddr {
+func NewIp(a, b, c, d byte, port int) net.TCPAddr {
 	ip := net.IPv4(a, b, c, d)
 	return net.TCPAddr{IP: ip, Port: port}
 }
