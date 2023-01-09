65670590746cad5a8081b32e14bb3489404c44f5
daida <daida459031925@163.com>
2023-01-09 18:11:46 +0800

    添加RuntimeExceptionTF

--- error/main.go
+++ error/main.go
@@ -5,3 +5,13 @@ import "errors"
 func New(errString string) error {
 	return errors.New(errString)
 }
+
+func RuntimeException(e error) {
+	panic(e)
+}
+
+func RuntimeExceptionTF(tf bool, e error) {
+	if tf {
+		panic(e)
+	}
+}
--- image/main.go
+++ image/main.go
@@ -1,6 +1,7 @@
 package image
 
 import (
+	err "github.com/daida459031925/common/error"
 	"github.com/daida459031925/common/fmt"
 	"github.com/daida459031925/common/result"
 	"image"
@@ -12,25 +13,17 @@ import (
 func GetImageFromNet(url string) result.Result {
 	return result.OkData(url).SetFunc(func(a any) any {
 		s, e := fmt.ParseUnPointer[string](a)
-		if e != nil {
-			panic(e)
-		}
+		err.RuntimeExceptionTF(e != nil, e)
 		res, e := http.Get(s)
-		if e != nil || res.StatusCode != result.OK {
-			panic(e)
-		}
+		err.RuntimeExceptionTF(e != nil || res.StatusCode != result.OK, e)
 		//若这个地方需要判断jpg和png则可以开启第二个方法进行逻辑计算
 		defer res.Body.Close()
 		m, e := jpeg.Decode(res.Body)
-		if e != nil {
-			panic(e)
-		}
+		err.RuntimeExceptionTF(e != nil, e)
 		return m
 	}).SetFunc(func(a any) any {
 		m, e := fmt.ParseUnPointer[image.Image](a)
-		if e != nil {
-			panic(e)
-		}
+		err.RuntimeExceptionTF(e != nil, e)
 		//可以做其他处理
 		return m
 	}).Exec()
